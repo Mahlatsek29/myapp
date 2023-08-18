21299d31bb9ed29db0d97f4e69b63be6ed29fca5
Mahlatsek29 <124674856+Mahlatsek29@users.noreply.github.com>
2023-08-18 14:32:01 +0200

    main

--- src/App.js
+++ src/App.js
@@ -15,10 +15,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Answer the Following Questions</h1>
+      <h1>Answer the Following Question</h1>
       <br />
       <form onSubmit={handleSubmit}>
         <div>What is your name?</div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="submit" />
+        <button onClick={handleDelete}>Delete</button>
+        <br/>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="submit" />
+        <button onClick={handleDelete}>Delete</button>
+        <br/>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <input type="submit" />

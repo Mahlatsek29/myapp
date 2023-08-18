04039ae5664a81b949ea9de142b3e241be316a69
Mahlatsek29 <124674856+Mahlatsek29@users.noreply.github.com>
2023-08-18 11:12:26 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("My name is " + name);
+  };
+
   return (
     <div className="App">
-      <h1> Hosting App</h1>
+      <h1>Answer the Following Questions</h1>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <div>What is your name?</div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
